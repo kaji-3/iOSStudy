49ab49fdd8d2439ad43c861d51c5122c7e30b66b
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-19 20:50:18 +0900

    chapter2 ~p.102 source

---
+++ VarEx/VarEx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VarEx
+//
+//  Created by y_kajikawa on 13/01/19.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

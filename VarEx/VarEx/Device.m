f97d6f0930d176d1c40ac3402a79689917b97c20
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-21 03:59:40 +0900

    chapter2 ~p.145 source

---
+++ VarEx/VarEx/Device.m
@@ -0,0 +1,13 @@
+//
+//  Device.m
+//  VarEx
+//
+//  Created by y_kajikawa on 13/01/19.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import "Device.h"
+
+@implementation Device
+
+@end

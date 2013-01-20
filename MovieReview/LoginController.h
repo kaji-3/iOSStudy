38206612507337903f5108c380a47fc536cb3bab
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-21 06:18:35 +0900

    Create Group Login, About

---
+++ MovieReview/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  LoginController.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/21.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginController : UIViewController
+
+@end
---
+++ MovieReview/LoginController.m
@@ -0,0 +1,43 @@
+//
+//  LoginController.m
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/21.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import "LoginController.h"
+
+@interface LoginController ()
+
+@end
+
+@implementation LoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+    NSLog(@"prepareForSeque>%@", [segue identifier]);
+}
+
+@end

079938ecc77b6532a1d457522e8e6d5910fae8ee
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-21 04:42:18 +0900

    non-chapter create original app

---
+++ MovieReview/MovieReview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/21.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

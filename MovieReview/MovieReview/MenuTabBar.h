0efe97abc3867ae707bf7910b854de37f0e0a44e
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-24 02:14:45 +0900

    tab buttun Controlller の実装

---
+++ MovieReview/MovieReview/MenuTabBar.h
@@ -0,0 +1,13 @@
+//
+//  MenuTabBar.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/24.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTabBar : UIViewController
+
+@end
---
+++ MovieReview/MovieReview/MenuTabBar.m
@@ -0,0 +1,38 @@
+//
+//  MenuTabBar.m
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/24.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import "MenuTabBar.h"
+
+@interface MenuTabBar ()
+
+@end
+
+@implementation MenuTabBar
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

6669f2081f9b1560cf819cd5462db4ebbd80e248
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-25 11:03:49 +0900

    WebBrowser内に WebView を紐付け

--- MovieReview/MovieReview/WebBrowser/WebBrowserController.h
+++ MovieReview/MovieReview/WebBrowser/WebBrowserController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface WebBrowserController : UIViewController
+@property (retain, nonatomic) IBOutlet UIWebView *webView;
 
 @end
--- MovieReview/MovieReview/WebBrowser/WebBrowserController.m
+++ MovieReview/MovieReview/WebBrowser/WebBrowserController.m
@@ -35,4 +35,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc {
+    [_webView release];
+    [super dealloc];
+}
 @end

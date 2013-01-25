38f16c0f2aa90508b488c1c149f89eac5bfe8491
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-25 10:55:01 +0900

    各ViewController追加、および、紐付け

---
+++ MovieReview/MovieReview/SortList/SortListController.m
@@ -0,0 +1,38 @@
+//
+//  SortListController.m
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/25.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import "SortListController.h"
+
+@interface SortListController ()
+
+@end
+
+@implementation SortListController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MovieReview/MovieReview/WebBrowser/WebBrowserController.h
@@ -0,0 +1,13 @@
+//
+//  WebBrowserController.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/25.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebBrowserController : UIViewController
+
+@end

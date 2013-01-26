7adabf81703d4fe6521b33b00eba19f63f9756b5
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-01-27 02:03:46 +0900

    CollectionViewへの表示実装

---
+++ MovieReview/MovieReview/SortList/NumberCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  NumberCollectionViewController.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/27.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NumberCollectionViewController : UICollectionViewController
+
+@end
---
+++ MovieReview/MovieReview/SortList/NumberCollectionViewController.m
@@ -0,0 +1,59 @@
+//
+//  NumberCollectionViewController.m
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/01/27.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import "NumberCollectionViewController.h"
+
+@interface NumberCollectionViewController ()
+
+@end
+
+@implementation NumberCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section
+{
+    //return datas.count;
+    return 10;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
+                                                                                forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor greenColor];
+    return cell;
+}
+
+@end

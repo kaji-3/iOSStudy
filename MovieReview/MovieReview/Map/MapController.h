5205dc5f017b1bd56b4715d980cd092ec46e556b
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-02-05 22:43:23 +0900

    地図表示タブの追加

---
+++ MovieReview/MovieReview/Map/MapController.h
@@ -0,0 +1,13 @@
+//
+//  MapController.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/02/05.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapController : UIViewController
+
+@end

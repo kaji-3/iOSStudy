a8c94bae3134a9499142862b0650ffb8a94a5652
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-02-07 01:47:00 +0900

    ViewController 用クラスの追加

---
+++ MovieReview/MovieReview/HatenaBookMark/HatenaBookmarkViewController.h
@@ -0,0 +1,13 @@
+//
+//  HatenaBookmarkViewController.h
+//  MovieReview
+//
+//  Created by y_kajikawa on 13/02/07.
+//  Copyright (c) 2013年 y_kajikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HatenaBookmarkViewController : UITableViewController
+
+@end

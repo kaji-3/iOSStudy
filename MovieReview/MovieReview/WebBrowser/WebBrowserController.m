1bc17bbd128f53558a29b440ee1b5387b132ac8b
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-02-06 12:34:21 +0900

    issue4 WebViewのインジケーターを画面中央に表示する 対応完了

--- MovieReview/MovieReview/WebBrowser/WebBrowserController.m
+++ MovieReview/MovieReview/WebBrowser/WebBrowserController.m
@@ -8,6 +8,7 @@
 
 #import "WebBrowserController.h"
 
+
 @interface WebBrowserController ()
 
 @end
@@ -18,7 +19,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+
     }
     return self;
 }
@@ -26,8 +27,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self initIndicator];
     [self loadWebPage:@"http://www.yahoo.co.jp"];
+}
+
+- (void) initIndicator {
+    // インジケーターインスタンスを作る
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    // タグ指定
+    indicator.tag = 1;
+    
+    // 画面の中央に表示するようにframeを変更する
+    float w = indicator.frame.size.width;
+    float h = indicator.frame.size.height;
+    float x = self.view.frame.size.width/2 - w/2;
+    float y = self.view.frame.size.height/2 - h/2;
+    indicator.frame = CGRectMake(x, y, w, h);
+    
+    // 止まったら終了
+    indicator.hidesWhenStopped = YES;
+    
+    // 雰囲気出すために背景を黒く半透明する
+    indicator.backgroundColor = [UIColor blackColor];
+    indicator.alpha = 0.5f;
+    indicator.color = [UIColor whiteColor];
+    
+    // サブビューに追加する
+    [self.view addSubview:indicator];
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,11 +75,21 @@
 
 // ページ読込開始時にインジケータをくるくるさせる
 -(void)webViewDidStartLoad:(UIWebView*)webView{
-    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    // ステータスヘッダーに出す場合
+    //[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    UIActivityIndicatorView *indicator = (UIActivityIndicatorView*) [self.view viewWithTag:1];
+    // クルクルと回し始める
+    [indicator startAnimating];
 }
 
 // ページ読込完了時にインジケータを非表示にする
 -(void)webViewDidFinishLoad:(UIWebView*)webView{
-    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    // ステータスヘッダーのを止める倍
+    //[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    UIActivityIndicatorView *indicator = (UIActivityIndicatorView*) [self.view viewWithTag:1];
+    // 停止させる
+    [indicator stopAnimating];
+    
 }
 @end

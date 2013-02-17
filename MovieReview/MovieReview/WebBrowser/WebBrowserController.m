97c820afed4e587e5c0053cc62cafa83e0a24ec5
Yasunari Kajikawa <y.kajikawa@gmail.com>
2013-02-18 00:27:47 +0900

    closed #5 スタイリシュなインジケータに変更

--- MovieReview/MovieReview/WebBrowser/WebBrowserController.m
+++ MovieReview/MovieReview/WebBrowser/WebBrowserController.m
@@ -7,7 +7,7 @@
 //
 
 #import "WebBrowserController.h"
-
+#import "SVProgressHUD.h"
 
 @interface WebBrowserController ()
 
@@ -28,38 +28,12 @@
 {
     [super viewDidLoad];
     [self initHeader];
-    [self initIndicator];
     [self loadWebPage:@"http://www.yahoo.co.jp"];
 }
 
 - (void)initHeader
 {
     [self.view addSubview:webHeaderView];
-}
-
-- (void) initIndicator {
-    // インジケーターインスタンスを作る
-    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-    // タグ指定
-    indicator.tag = 1;
-    
-    // 画面の中央に表示するようにframeを変更する
-    float w = indicator.frame.size.width;
-    float h = indicator.frame.size.height;
-    float x = self.view.frame.size.width/2 - w/2;
-    float y = self.view.frame.size.height/2 - h/2;
-    indicator.frame = CGRectMake(x, y, w, h);
-    
-    // 止まったら終了
-    indicator.hidesWhenStopped = YES;
-    
-    // 雰囲気出すために背景を黒く半透明する
-    indicator.backgroundColor = [UIColor blackColor];
-    indicator.alpha = 0.5f;
-    indicator.color = [UIColor whiteColor];
-    
-    // サブビューに追加する
-    [self.view addSubview:indicator];
 }
 
 - (void)didReceiveMemoryWarning
@@ -81,21 +55,15 @@
 
 // ページ読込開始時にインジケータをくるくるさせる
 -(void)webViewDidStartLoad:(UIWebView*)webView{
-    // ステータスヘッダーに出す場合
-    //[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-    
-    UIActivityIndicatorView *indicator = (UIActivityIndicatorView*) [self.view viewWithTag:1];
-    // クルクルと回し始める
-    [indicator startAnimating];
+    //[SVProgressHUD dismiss]; // 消す
+    [SVProgressHUD show]; // ぐるぐる表示
 }
 
 // ページ読込完了時にインジケータを非表示にする
 -(void)webViewDidFinishLoad:(UIWebView*)webView{
-    // ステータスヘッダーのを止める倍
-    //[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-    UIActivityIndicatorView *indicator = (UIActivityIndicatorView*) [self.view viewWithTag:1];
-    // 停止させる
-    [indicator stopAnimating];
+    
+    [SVProgressHUD dismiss]; // 消す
+
     
 }
 - (IBAction)backButtonTouchDown:(id)sender {
